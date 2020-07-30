50e142f663da57763c47e98953d2b9f03e29a31e
Anker2898 <ghma28@outlook.com>
2020-07-29 20:29:46 -0500

    Base de Datos

--- Sistema/shared/formMensaje.php
+++ Sistema/shared/formMensaje.php
@@ -7,7 +7,7 @@ class formMensaje{
         <!DOCTYPE html>
         <html lang="en">
             <head>
-                <title></title>
+                <title>Mensaje</title>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
