cd81976da2cc8e26c8ca6371fc0a9fe00aa08c30
Mildark10 <sonix-141@hotmail.com>
2020-08-01 19:07:20 -0500

    go

--- Recursos/Plantillas/Vista/FormEditarHerramienta.php
+++ Recursos/Plantillas/Vista/FormEditarHerramienta.php
@@ -16,7 +16,7 @@ include_once("Plantilla/cabezera.php");
 <img src="../assets/img/user.png" class="NavBar-Nav-icon" alt="User">
 <p class="full-width"><small><b>Almacener@:<br>Lisbeth</b></h1></small></p><br><br>
 <br>
-
+        
 	    <div class="form-group">
             <input class="form-control item" type="text" name="" maxlength="100" minlength="4"  id="" placeholder="Nombre" required>
         </div>
@@ -26,7 +26,7 @@ include_once("Plantilla/cabezera.php");
         <div class="form-group">
         <textarea class="form-control item" type="text" name=""  style="WIDTH: 340px; HEIGHT: 98px" maxlength="200" minlength="4" size=32  id="" placeholder="Detalle" required></textarea>
         
-        </div>
+        </div>  
         <form action="get.php" method="POST">
         <div class="form-group">
             <button class="btn btn-primary btn-block create-account" type="submit"> Modificar</button>
