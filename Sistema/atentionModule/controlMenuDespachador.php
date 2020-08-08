84f81a3a0e99e727c3ddfc3b9061c8978ded981b
Flavio <favio789@gmail.com>
2020-08-07 23:38:07 -0500

    mostrar solicitudes

--- Sistema/atentionModule/controlMenuDespachador.php
+++ Sistema/atentionModule/controlMenuDespachador.php
@@ -2,17 +2,20 @@
 
 include_once("formRegistro.php");
 include_once("formListaSolicitud.php");
+include_once("../model/solicitud.php");
 
 class controlMenuDespachador{
 
     public function iniciarSolicitudes(){
         $objForm = new formListaSolicitud;
-        $objForm -> formListaSolicitudShow();
+        $objGestion = new solicitud;
+        $objForm -> formListaSolicitudShow($objGestion -> obtenerSolicitudTodo());
     }
 
     public function iniciarRegistroSalida(){
         $objForm = new formRegistro;
-        $objForm -> formRegistroShow();
+        $objGestion = new solicitud;
+        $objForm -> formRegistroShow($objGestion -> obtenerRegistroSalidaTodo());
     }
 
 }
